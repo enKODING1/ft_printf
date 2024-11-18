5bffd21980335fd1da1db971eb695fc54d424705
Seongjin Kang <skang@c3r17s4.42gyeongsan.kr>
2024-11-18 20:53:54 +0900

    upload

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: skang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:34:48 by skang             #+#    #+#             */
-/*   Updated: 2024/11/18 14:38:46 by skang            ###   ########.fr       */
+/*   Updated: 2024/11/18 20:53:39 by skang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,45 @@
 #include "ft_printf.h"
 
 
+int valid_specifier(char specifier)
+{
+   char valid_specifier_array[9];
+   int   index;
+
+   ft_strlcpy(valid_specifier_array, "cspdiuxX%", 10);
+   index = 0;
+   while (valid_specifier_array[index] != '\0')
+   {
+         if (specifier == valid_specifier_array[index])
+            return 1;
+         index++;
+   }
+   return 0;
+}
+
+int  specifier_dispatcher(char specifier)
+{
+         
+}
+
 int ft_printf(const char *format, ...)
 {
    va_list arg_ptr;
-   char *arg;
+   int index;
+   
    va_start(arg_ptr, format);
-   arg = va_arg(arg_ptr, char *);
-   printf("first: [%s] \n", format);
-   printf("second: [%s] \n", arg);
+   index = 0;
+   while (format[index] != '\0' || index < ft_strlen(format)) 
+   {
+      if (format[index] == '%' && valid_specifier(format[index + 1]))
+      {
+
+         index += 2;
+         
+         continue;
+      }
+      ft_putchar_fd(format[index], 1);
+      index++;
+   }
    return 0;
 }
